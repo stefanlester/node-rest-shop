3292749ff68f44a01fd9fbbcb97e066351b9ace8
stefanlester <38575516+stefanlester@users.noreply.github.com>
2020-12-09 07:16:20 +0100

    Done

--- api/models/order.js
+++ api/models/order.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const productSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name:  { type: String, required: true },
-    price: { type: Number, required: true }
+    product: { type:mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, default: 1 }
 })
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model('Order', orderSchema)
