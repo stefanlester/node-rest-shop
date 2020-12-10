4f07a14852037fd0429f5ddb0d4d8d1c0ce3e855
stefanlester <38575516+stefanlester@users.noreply.github.com>
2020-12-10 05:40:46 +0100

    check

--- app.js
+++ app.js
@@ -6,51 +6,48 @@ const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/product')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect("mongodb://localhost/3000", {useMongoClient:true}); //hmm
 
 mongoose.Promise = global.Promise;
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// CORS Handler to prevent cross site request forgery(csrf)
-// Also prevents API from external access
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); //chnage "*" to domains you want access to
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-//Routes which should handle requests
-app.use('/products', productRoutes)
-app.use('/orders', orderRoutes)
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
-
-//error handler for web server 1
 app.use((req, res, next) => {
-    const error = new Error('not found')
-    error.status = 404 
-    next(error)
-})
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
-//protecting routes without API routes
-//error handler for web server 1.1
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
-module.exports = app
+module.exports = app;
